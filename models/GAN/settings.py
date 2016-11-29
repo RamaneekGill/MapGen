51ae6ecfa8d484fe3e79849d726a667726a5b163
RamaneekGill <ramaneek.gill@hotmail.com>
2016-11-28 20:49:39 -0500

    Start working on a GAN model

---
+++ models/GAN/settings.py
@@ -0,0 +1,33 @@
+random_seed = 1337
+
+dropout_rate = 0.1
+
+learning_rate = 0.001
+
+batch_size = 1
+
+num_generator_channels = 64
+
+num_discriminator_channels = 64
+
+num_input_channels = 3
+
+num_output_channels = 3
+
+beta1 = 0.5 # beta for Adam optimizer
+
+lamda = 100 # weight for L1 regularization
+
+image_width = 200
+
+image_height = 200
+
+kernel_height = 4
+
+kernel_width = 4
+
+kernel_stride = 2
+
+padding = 1
+
+relu_alpha = 0.2 # for LeakyReLU
