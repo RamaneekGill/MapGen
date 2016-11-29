76d29dedf98cbcca65821f97e8939005288f6782
Charles Huang <charleshche@gmail.com>
2016-11-28 20:47:04 -0500

    Implement load_data

--- models/GAN/train.py
+++ models/GAN/train.py
@@ -1,7 +1,27 @@
+import os
+from skimage import io
+
 def load_data(path):
 	# Implement a function that loads data that the transformer saved
 	# return data
-	pass
+	fn = os.listdir(inputdir)
+	sat_arr = []
+	map_arr = []
+
+	i = 0
+    j = 0
+    while i < len(fn):
+        split = fn[i].split('_')
+        if split[1] == 'map':
+            j += 1
+            filename = os.path.join(inputdir, fn[i])
+            output_arr.append(io.imread(filename))
+            split[1] = 'satellite'
+            filename = os.path.join(inputdir, '_'.join(split))
+            input_arr.append(io.imread(filename))
+        i += 1
+
+	return [sat_arr, map_arr]
 
 def train(model, training_data, validation_data, **kwargs):
 	# Implement a function that takes a model and data and trains it
