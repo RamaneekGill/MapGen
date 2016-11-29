b65ce43b280061c3a215f87f28117f200413129b
Charles Huang <charleshche@gmail.com>
2016-11-28 21:00:54 -0500

    Fix variable errors

--- models/GAN/train.py
+++ models/GAN/train.py
@@ -4,24 +4,24 @@ from skimage import io
 def load_data(path):
 	# Implement a function that loads data that the transformer saved
 	# return data
-	fn = os.listdir(inputdir)
-	sat_arr = []
-	map_arr = []
+    fn = os.listdir(path)
+    sat_arr = []
+    map_arr = []
 
-	i = 0
+    i = 0
     j = 0
     while i < len(fn):
         split = fn[i].split('_')
         if split[1] == 'map':
             j += 1
-            filename = os.path.join(inputdir, fn[i])
-            output_arr.append(io.imread(filename))
+            filename = os.path.join(path, fn[i])
+            map_arr.append(io.imread(filename))
             split[1] = 'satellite'
-            filename = os.path.join(inputdir, '_'.join(split))
-            input_arr.append(io.imread(filename))
+            filename = os.path.join(path, '_'.join(split))
+            sat_arr.append(io.imread(filename))
         i += 1
 
-	return [sat_arr, map_arr]
+    return [sat_arr, map_arr]
 
 def train(model, training_data, validation_data, **kwargs):
 	# Implement a function that takes a model and data and trains it
